9f54208e36269be8fed0da7bc13fe6baa3e0aca4
Yeohoon <yhoon.yoon@gmail.com>
2022-01-17 22:54:56 -0500

    commit tree 27f5b700ccacc535f59e62e4f0d8588a66014bc8

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
 const returnFirstTwoDrivers = function(array){
   return array.slice(0,2);
-}
-
-const returnLastTwoDrivers = function (array){
-  return array.slice((array.length-2), (array.length));
-}
-
-const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers ];
-
-function createFareMultiplier(number){
-   return function fareNumber (fare){
-     return fare * number; 
-    }
-  };
-
-
-const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
-
-function selectDifferentDrivers(arrayOfDrivers, func){
-  if(func === returnFirstTwoDrivers){
-    return func(returnFirstTwoDrivers(arrayOfDrivers));
-  }else{
-    return func(returnLastTwoDrivers(arrayOfDrivers));
   }
+
+const returnLAstTowDrivers= function(array){
+  return array.slice((array.length-2), (array.length))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const returnFirstTwoDrivers = function(array){
+//   return array.slice(0,2);
+// }
+
+// const returnLastTwoDrivers = function (array){
+//   return array.slice((array.length-2), (array.length));
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers ];
+
+// function createFareMultiplier(number){
+//    return function fareNumber (fare){
+//      return fare * number; 
+//     }
+//   };
+
+
+// const fareDoubler = createFareMultiplier(2);
+// const fareTripler = createFareMultiplier(3);
+
+// function selectDifferentDrivers(arrayOfDrivers, func){
+//   if(func === returnFirstTwoDrivers){
+//     return func(returnFirstTwoDrivers(arrayOfDrivers));
+//   }else{
+//     return func(returnLastTwoDrivers(arrayOfDrivers));
+//   }
+// }
